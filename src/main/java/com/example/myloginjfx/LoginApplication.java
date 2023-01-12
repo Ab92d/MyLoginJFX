29e25d708bb11781944d1e886ad99f14aa9b1965
Amy B <110386059+Ab92d@users.noreply.github.com>
2023-01-12 22:15:11 +0000

    - added comments - changed size of window when launched for better fit - changed title message

--- src/main/java/com/example/myloginjfx/LoginApplication.java
+++ src/main/java/com/example/myloginjfx/LoginApplication.java
@@ -8,12 +8,14 @@ import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
+// This class launches the application and sets the scene to the login-view
 public class LoginApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 200, 400);
-        stage.setTitle("Login");
+        Scene scene = new Scene(fxmlLoader.load(), 480 , 480);
+        // sets the title (bar at the top of window)
+        stage.setTitle("Welcome to the Login Page");
         stage.initStyle(StageStyle.UTILITY);  // removes minimise and maximise options on window
         stage.setScene(scene);
         stage.show();
