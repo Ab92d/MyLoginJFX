609313a1cfc5164aae78b66d6f5512769ecea5a8
Amy B <110386059+Ab92d@users.noreply.github.com>
2023-01-12 17:11:55 +0000

    launches application

---
+++ src/main/java/com/example/myloginjfx/loggedInController.java
@@ -0,0 +1,9 @@
+package com.example.myloginjfx;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+public class loggedInController {
+
+}
